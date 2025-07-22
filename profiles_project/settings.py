a0952d6c664f923d8331edf0c7854aa1b12852f5
JperezRi03 <juan.perez@lgcns.com>
2025-07-22 09:31:45 -0500

    Enable Django Admin for user profile model

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles_api',
-    #'rest_framework',
-    #'rest_framework.authtoken',
+    'rest_framework',
+    'rest_framework.authtoken',
 
 ]
 
